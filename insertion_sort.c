43ccee56d20caa47fe5cbd963847be7dd9359940
Sujathamoger <34388709+Sujathamoger@users.noreply.github.com>
2021-03-22 15:06:13 +0530

    Update insertion_sort.c

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,43 +1,38 @@
 #include <math.h>
 #include <stdio.h>
 
-void swap(int *a,int *b)
-{
-	int *temp;
-	*temp=*a;
-	*a=*b;
-	*b=*temp;
-}
-void insertion_sort(int *arr1[10],int n)
+void insertion_sort(int *arr,int n)
 {
 	int i,j,key;
 	for(i=1;i<n;i++)
 	{
-		key = arr1[i];
+		key = arr[i];
 		j=i-1;
-		while(j>=0)
+		while(j>=0 && arr[j]>key)
 		{
-			if(arr1[j] > key)
-			{
-				swap(&arr1[j],&arr1[i]);
-			}
+			arr[j+1] = arr[j];
 			j--;
 		}
+		arr[j+1] = key;
 	}
 }
 int main()
 {
-	int arr1[10],arr2[10];
+	int arr[6] = {7,3,5,4,2,6};
  	int i,n;
- 	printf("Enter the numbers of elements\n");
- 	scanf("%d",&n);
-	printf("Enter the numbers\n");
+	n=6;
+	
+	printf("Array before sorting\n");
 	for(i=0;i<n;i++)
 	{
-		scanf("%d",&arr1[i]);
+		printf("%d\t",arr[i]);
 	}
+	insertion_sort(arr,n);	
 	
-	insertion_sort(&arr1,n);
-	
+	printf("\n\nArray after sorting\n");
+	for(i=0;i<n;i++)
+	{
+		printf("%d\t",arr[i]);
+	}
 	return 0;
 }
