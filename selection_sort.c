542a36e1fbb08848c53dab3c0c1b248864a4fd33
Sujathamoger <34388709+Sujathamoger@users.noreply.github.com>
2021-03-22 13:56:23 +0530

    Update selection_sort.c

--- selection_sort.c
+++ selection_sort.c
@@ -1,65 +1,38 @@
 #include <stdio.h>
 #include<stdlib.h>
-/*****selection sort
 
-arr[] = 64 25 12 22 11
+/*Finding minimum element and swapping with the index element*/
 
-// Find the minimum element in arr[0...4]
-// and place it at beginning
-11 25 12 22 64
-
-// Find the minimum element in arr[1...4]
-// and place it at beginning of arr[1...4]
-11 12 25 22 64
-
-// Find the minimum element in arr[2...4]
-// and place it at beginning of arr[2...4]
-11 12 22 25 64
-
-// Find the minimum element in arr[3...4]
-// and place it at beginning of arr[3...4]
-11 12 22 25 64 
-
-****/
-
-void swap(int *a,int *b)
-{
-	int temp=0;
-	temp=*a;
-	*a=*b;
-	*b=temp;
-	return ;
-}
 int main()
 {
 	int arr[6]={6,2,4,9,1,3};
-	int min_index=0;
-
-	int i=0,n;
+	int i,j,min,n,temp;
 	
 	n=sizeof(arr)/sizeof(arr[0]);
 	
 	printf("%d\n",n);
 	for(i=0;i<n-1;i++)
 	{
-		min_index=i;
-		for(int j=i+1;j<n;j++)
+		min=i;
+		for(j=i+1;j<n;j++)                    /*Inner for loop to find out what is the minimum element*/
 		{
-			printf("arr[min_index],arr[j]=%d %d\n",arr[min_index],arr[j]);
-			if(arr[j]<arr[min_index])
+			printf("arr[min_index],arr[j]=%d %d\n",arr[min],arr[j]);
+			if(arr[j] < arr[min])
 			{
-				swap(&arr[min_index],&arr[j]);
+				min = j;
 			}
 		}
 		
+		temp = arr[i];                    /*Swapping minimum element & index element*/
+		arr[i] = arr[min];
+		arr[min] = temp;
+		
 	}
+	printf("\n");
 	for(i=0;i<n;i++)
 	{
 		printf("%d\t",arr[i]);
 	}
-	
-
-
 	
 	return 0;
 }
